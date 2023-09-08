667623432f339d9c414e034f01adc4d29639920f
Samuel Grenier <2068365@cstjean.qc.ca>
2023-09-07 22:23:39 -0400

    etape4

---
+++ .bashrc
@@ -0,0 +1,8 @@
+echo "Bonjour $USERNAME" 
+
+if [ "$USERNAME" = "samuel" ]; then
+	PS1="\e[0;32m\w\e[m\$ "
+fi
+
+source ./.aliases
+source ./.functions
---
+++ Install.sh
@@ -0,0 +1,11 @@
+mkdir ./bin
+mkdir ./progSys
+mkdir ./projets
+
+sudo apt install git
+sudo apt install vim
+sudo apt install gcc
+sudo apt install gdb
+sudo apt install python3
+sudo apt install python3-pip
+sudo apt install python3-venv
